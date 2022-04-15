c3835f73feebb4929f070cf0254a43179904bf0f
Mihai <96mihai.ionescu@gmail.com>
2022-04-15 13:32:44 +0300

    Initial commit

---
+++ app/src/main/java/com/mi23/ddsmappmci/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.mi23.ddsmappmci.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
