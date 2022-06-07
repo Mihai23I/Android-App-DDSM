fe0568b1e1335b98489f6a225720e2068840f0d9
Mihai <96mihai.ionescu@gmail.com>
2022-06-07 11:15:17 +0300

    add markers, layout

--- app/src/main/java/com/mi23/ddsmappmci/ui/Maps/MapsFragment.java
+++ app/src/main/java/com/mi23/ddsmappmci/ui/Maps/MapsFragment.java
@@ -7,6 +7,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -20,6 +22,10 @@ import android.view.ViewGroup;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.mi23.ddsmappmci.R;
@@ -65,8 +71,20 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback{
     public void onMapReady(@NonNull GoogleMap map) {
         googleMap = map;
 
-        LatLng Unibuc = new LatLng(44.435578, 26.099525);
+        LatLng Unibuc = new LatLng(44.435601, 26.100917);
         googleMap.addMarker(new MarkerOptions().position(Unibuc).title("Unibuc"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(44.427500, 26.087500)).title("Palatul Parlamentului"));
+
+        googleMap.addCircle(new CircleOptions().center(Unibuc).radius(100));
+
+
+//        googleMap.addGroundOverlay(
+//                new GroundOverlayOptions()
+//                        .position(new LatLng(44.427500, 26.087500), 100f, 100f)
+//                        .image()
+//                        .clickable(true)
+//        )
+
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(Unibuc));
         googleMap.setMinZoomPreference(15.0f);
         this.googleMap = googleMap;
