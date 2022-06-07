54b3ffb1794f5aeb7cec5d5cfc3bcd5cdba7ff9f
Mihai <96mihai.ionescu@gmail.com>
2022-06-07 10:45:07 +0300

    Add maps with marker

--- app/src/main/java/com/mi23/ddsmappmci/ui/Maps/MapsFragment.java
+++ app/src/main/java/com/mi23/ddsmappmci/ui/Maps/MapsFragment.java
@@ -2,6 +2,11 @@ package com.mi23.ddsmappmci.ui.Maps;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,9 +17,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mi23.ddsmappmci.R;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements OnMapReadyCallback{
 
     private MapsViewModel mViewModel;
 
@@ -22,10 +32,26 @@ public class MapsFragment extends Fragment {
         return new MapsFragment();
     }
 
+    private MapView mapView;
+    private GoogleMap googleMap;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.maps_fragment, container, false);
+
+        View view = inflater.inflate(R.layout.maps_fragment, container, false);
+
+        return view;
+        //44.435578, 26.099525
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = (MapView) view.findViewById(R.id.maps);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
     }
 
     @Override
@@ -33,6 +59,17 @@ public class MapsFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MapsViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap map) {
+        googleMap = map;
+
+        LatLng Unibuc = new LatLng(44.435578, 26.099525);
+        googleMap.addMarker(new MarkerOptions().position(Unibuc).title("Unibuc"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(Unibuc));
+        googleMap.setMinZoomPreference(15.0f);
+        this.googleMap = googleMap;
     }
 
 }
