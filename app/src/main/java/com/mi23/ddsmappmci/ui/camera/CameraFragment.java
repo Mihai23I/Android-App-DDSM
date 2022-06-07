21583f030252b9ffe101baf255c3a622d06dec04
Mihai <96mihai.ionescu@gmail.com>
2022-06-07 09:00:34 +0300

    Added camera button to menu

---
+++ app/src/main/java/com/mi23/ddsmappmci/ui/camera/CameraFragment.java
@@ -0,0 +1,38 @@
+package com.mi23.ddsmappmci.ui.camera;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mi23.ddsmappmci.R;
+
+public class CameraFragment extends Fragment {
+
+    private CameraViewModel mViewModel;
+
+    public static CameraFragment newInstance() {
+        return new CameraFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.camera_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CameraViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/mi23/ddsmappmci/ui/camera/CameraViewModel.java
@@ -0,0 +1,7 @@
+package com.mi23.ddsmappmci.ui.camera;
+
+import androidx.lifecycle.ViewModel;
+
+public class CameraViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
