0a8ef3870502bbd5ac4980d49437a3c6efc9d314
Mihai <96mihai.ionescu@gmail.com>
2022-06-07 09:32:24 +0300

    Add Camera task

--- app/src/main/java/com/mi23/ddsmappmci/ui/camera/CameraFragment.java
+++ app/src/main/java/com/mi23/ddsmappmci/ui/camera/CameraFragment.java
@@ -1,31 +1,85 @@
 package com.mi23.ddsmappmci.ui.camera;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.mi23.ddsmappmci.R;
 
 public class CameraFragment extends Fragment {
 
     private CameraViewModel mViewModel;
+    ImageView camera_view;
+    Button cam_open;
 
     public static CameraFragment newInstance() {
         return new CameraFragment();
     }
 
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.camera_fragment, container, false);
+
+        View view = inflater.inflate(R.layout.camera_fragment, container, false);
+        camera_view = view.findViewById(R.id.camera_view);
+        cam_open = view.findViewById(R.id.cam_open);
+
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[] {
+                    Manifest.permission.CAMERA
+            }, 101);
+        }
+
+//        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+//                new ActivityResultContracts.StartActivityForResult(),
+//                new ActivityResultCallback<ActivityResult>() {
+//                    @Override
+//                    public void onActivityResult(ActivityResult result) {
+//                        if (result.getResultCode() == Activity.RESULT_OK) {
+//                            // There are no request codes
+//                            Intent data = result.getData();
+//
+//                            // Request
+//                            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+//                            camera_view.setImageBitmap(captureImage);
+//                        }
+//                    }
+//                });
+
+
+
+        cam_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 101);
+            }
+        });
+
+        return view;
     }
 
     @Override
@@ -33,6 +87,16 @@ public class CameraFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(CameraViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101){
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            camera_view.setImageBitmap(captureImage);
+        }
     }
 
 }
